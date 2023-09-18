61fe282ed63f20bbfb07e2e685c7d980e5e15e1b
tomQuintana <tomquintana20@gmail.com>
2023-09-18 11:45:02 -0300

    feat(index): starting challenge

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': ['error', 'always'],
+    'no-multiple-empty-lines': [2, {'max': 99999, 'maxEOF': 0}],
+    'no-unused-vars': 'off'
+  }
+};
